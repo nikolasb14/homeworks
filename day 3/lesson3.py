b21696a434bc0abc4be3b27c992bf3fccec01e0a
Николас Бобохидзе <nikolasbobokhidze14@gmail.com>
2023-07-14 22:33:41 +0400

    fourth commit

---
+++ day 3/lesson3.py
@@ -0,0 +1,50 @@
+name = "luka
+surname -> "forchxidze"
+age = (15
+print(name + surname + age)
+print(name)
+print(surname)
+print(age)
+print(name + surname)
+boys -> 15
+girls = 17
+classroom -> (boys + girls
+print(classroom)
+name = "mari
+age = 10
+print(name + age)
+from turtle import
+speed(122
+width(9)
+color("yellow)
+forward(200
+left(90)
+forward(\200)
+left(90)
+forward(200)
+left(90)
+forwr(200)
+left(90)
+name = "nikolas
+surname -> "bobokhidze"
+age = 15
+print("my name is  + name + str(surname) + "." + " I am " + str(age + " years old.")
+gios_apples = 10
+sabas apples = 8
+ninos-apples = 5
+maris_apples = 12
+print(gios_apples)
+print(sabas_apples)
+print(ninos_apples)
+print(maris_apples)
+#calculation of all apples
+print(gios_apples + sabas apples + ninos-apples + maris_apples)
+message = "hi my name is"
+print(message + name)
+print(Message + NAME)
+print(message)
+print(name)
+first_candedate = nika
+second_candedate = luka
+print(Fisrt_candidate)
+print(second_CANDIDATE)
