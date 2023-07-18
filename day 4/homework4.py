bb6d2dc49a4f6166009d523641904075acd4d7a8
Николас Бобохидзе <nikolasbobokhidze14@gmail.com>
2023-07-18 15:58:06 +0400

    fourth commit

---
+++ day 4/homework4.py
@@ -0,0 +1,4 @@
+year_of_birth = input("Enter your birht year: ")
+year_to_travel = input("Enter the year you want to travel to: ")
+print(int(year_to_travel) - int(year_of_birth))
+
---
+++ day 4/lesson4.py
@@ -0,0 +1,8 @@
+name = input("Your name: ")
+surname = input("Your surname: ")
+age = input("Your age: ")
+print(name + " " + surname + " " + str(age))
+
+#ramdeni wlis aris
+#ramdeni weli unda droshi mogzauroba
+#ramdeni wlis iqneba mogzaurobis shemdeg
