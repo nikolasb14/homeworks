4a90d06d0f57822d8efe02ce0dc6ea96a1aa0b3c
Николас Бобохидзе <nikolasbobokhidze14@gmail.com>
2023-07-21 22:57:45 +0400

    next

---
+++ day 5/homework.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+birthyear = input("Enter the year you were born in: ")
+age = (2023 - int(birthyear))
+country = input("Enter the country you live in: ")
+hometown = input("Enter your hometown: ")
+
+print(name + " " + surname + ", " + str(age) + ", " + country + ", " + hometown)
+
+
+
