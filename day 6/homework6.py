276f525a9194dc62666e541723e1d8798cbc6022
Николас Бобохидзе <nikolasbobokhidze14@gmail.com>
2023-07-25 17:11:37 +0400

    next

---
+++ day 6/homework6.py
@@ -0,0 +1,5 @@
+age = input("Enter your age: ")
+print(int(age) > 18)
+fathers_age = input("Enter your father's age: ")
+your_age = input("Enter your age: ")
+print(int(fathers_age) + int(your_age) > 60)
