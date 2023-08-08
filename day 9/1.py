8aa5f316d2d764bd1bba9a22da0e9fb36021bacb
Николас Бобохидзе <nikolasbobokhidze14@gmail.com>
2023-08-08 13:55:52 +0400

    first commit

---
+++ day 9/1.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age: "))
+i = 0
+while i <= age:
+    print("gilocav goashi shemosvlas")
+    i = i + 8
+    x = 0
+    while x <= age:
+        print("gilocav shen gaxdi", x, "wlis" )
+        x = x + 1
+
+
+
+
+
---
+++ day 9/homework1.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+i = 0
+while i <= age:
+    print("გილოცავ, შენ გახდი", i, "წლის!" )
+    i = i + 1
+#goashi shemosvlis mesiji ver gavige yovel merveze rogor gavushva
