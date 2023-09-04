c72a8d1e97ab7b1ccd74642d78a77a3e3a7e648b
Николас Бобохидзе <nikolasbobokhidze14@gmail.com>
2023-09-04 14:01:35 +0400

    next

---
+++ day 14/homework2.py
@@ -0,0 +1,10 @@
+side = int(input("Enter length of the side of the square:"))
+
+def square():
+    perimeter = side * 4
+    area = side * side
+    print(perimeter)
+    print(area)
+
+
+square()
---
+++ day 14/homework3.py
@@ -0,0 +1,10 @@
+side1 = int(input("Enter length of first side of the triangle:"))
+side2 = int(input("Enter length of second side of the triangle:"))
+side3 = int(input("Enter length of third side of the triangle:"))
+
+def triangle():
+    perimeter = side1 + side2 + side3
+    print(perimeter)
+
+
+triangle()
