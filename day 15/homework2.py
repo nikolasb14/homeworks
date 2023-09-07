70c93c7dc55ff095af64b16c4c67e6b498526472
Николас Бобохидзе <nikolasbobokhidze14@gmail.com>
2023-09-07 21:24:20 +0400

    next

--- day 15/homework2.py
+++ day 15/homework2.py
@@ -4,17 +4,25 @@ import random
 
 crew_members = ['ლაზარე ღოღობერიძე', 'ლუკა თელია', 'ლექსო პატარაია', 'თორნიკე ქარელიძე', 'ნინო ოჩიგავა', 'ლუკა გამრეკლიძე', 'ნიკო ქუთათელაძე', 'ნიკოლას ბობოხიძე',]
 
-for i in range(1, 4):
-    winner = random.choice(crew_members)
-    print(winner, "კარგად სწავლობს")
-    crew_members.remove(winner)
+winner1 = random.choice(crew_members)
+winner2 = random.choice(crew_members)
+winner3 = random.choice(crew_members)
 
 
-crew_members_name = ['ლაზარე', 'ლუკა', 'ლექსო', 'თორნიკე', 'ნინო', 'ლუკა', 'ნიკო', 'ნიკოლას',]
-
-if crew_members_name[-1] == "ი":
-    print("cool")
+if winner1[-1] == "ი":
+    print(winner1, "is cool")
 else:
-    print('No ones name ended with "ი", but they are cool anyways')
+    print(winner1)
+
+if winner2[-1] == "ი":
+    print(winner2, "is cool")
+else:
+    print(winner2)
+
+if winner3[-1] == "ი":
+    print(winner3, "is cool")
+else:
+    print(winner3)
 
 
+    
